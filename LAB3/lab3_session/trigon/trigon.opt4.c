b7a202521b73d8c019c7c9a5a83b201aa4c557fc
El vostre nom <usuari@domini.com>
2021-03-24 16:58:13 +0100

    LAB3 acabant

--- LAB3/lab3_session/trigon/trigon.opt4.c
+++ LAB3/lab3_session/trigon/trigon.opt4.c
@@ -12,19 +12,25 @@ int main(int argc, char *argv[])
 {
 	unsigned int i, r, j, n, a;
 	double d;
+	double memo_cos[PUNTS], memo_sin[PUNTS];
 	double xy[PUNTS*2];
 	if (argc == 1) n = N; else n = atoi(argv[1]);
 
+
+	for(i=0, d=0; i<PUNTS; i++){
+			memo_cos[i] = cos(d);
+			memo_sin[i] = sin(d);
+			d += 2*M_PI/PUNTS;
+	}	
 
 	srand(0);
 	for (i=0; i<n; i++)
 	{
 		r = rand();
-		for (j=0, d=0, a=0; j<PUNTS; j++, a+=2)
+		for (j=0, a=0; j<PUNTS; j++, a+=2)
 		{
-			xy[a] = r*cos(d);
-			xy[a+1] = r*sin(d);
-			d += 2*M_PI/PUNTS;
+			xy[a] = r*memo_cos[j];
+			xy[a+1] = r*memo_sin[j];
 		}
 		fwrite(xy, sizeof(double), 2*PUNTS, stdout);
 	}
