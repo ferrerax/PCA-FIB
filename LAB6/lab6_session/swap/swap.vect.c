3adbbac1915bb9abc67f24f54b71589ae75dc48f
Jordi Garcia Aguilar <jordi.garcia.aguilar@a5s113pc54.fib.upc.es>
2021-05-26 12:38:00 +0200

    ale

--- LAB6/lab6_session/swap/swap.vect.c
+++ LAB6/lab6_session/swap/swap.vect.c
@@ -5,42 +5,41 @@
 #include <stdlib.h>
 #include <tmmintrin.h>
  
-void panic(char *miss)
-{
-        if (errno != 0) perror(miss);
-        else fprintf(stderr, "%s\n", miss);
- 
-        exit(1);
-}
+
+#define COUNT 64
  
 int main(int argc, char *argv[])
 {
-    int count = 100;
-    if (argc == 2) count = atoi(argv[1]);
 
     __m128i mask;
-    __m128i *c1, *c1_out;
 
-    posix_memalign((void **)&c1,16,count);
-    posix_memalign((void **)&c1_out,16,count);
- 
-    int i;
-    // VECTORIAL
+    __m128i c1[COUNT], c1_out[COUNT];
+
     mask = _mm_set_epi8(0xE, 0xF, 0xC, 0xD, 0xA, 0xB, 0x8, 0x9, 0x6, 0x7, 0x4, 0x5, 0x2, 0x3, 0x0, 0x1);
-    for(i=0; i < (count>>4); i++)
-      c1_out[i] = _mm_shuffle_epi8(c1[i], mask);
+    int n1;
 
-    // EPILOG
-    for (i=i*16; i<count-1; i+=2) {
-      ((char *)c1_out)[i]     = ((char *)c1)[i+1];
-      ((char *)c1_out)[i+1]   = ((char *)c1)[i];
+    while( (n1 = read(0,c1,COUNT*16)) > 0)
+    {
+      int i;
+      // VECTORIAL
+      for(i=0; i < (n1>>4)-3; i+=4) {
+        c1_out[i] = _mm_shuffle_epi8(c1[i], mask);
+        c1_out[i+1] = _mm_shuffle_epi8(c1[i+1], mask);
+        c1_out[i+2] = _mm_shuffle_epi8(c1[i+2], mask);
+        c1_out[i+3] = _mm_shuffle_epi8(c1[i+3], mask);
+      }
+      for(; i < (n1>>4); i++) 
+        c1_out[i] = _mm_shuffle_epi8(c1[i], mask);
+
+      // EPILOG ESCALAR
+      for (i=i*16; i<n1-1; i+=2) {
+        ((char *)c1_out)[i]     = ((char *)c1)[i+1];
+        ((char *)c1_out)[i+1]   = ((char *)c1)[i];
+      }
+      if (i < n1)
+         ((char *)c1_out)[i] = ((char *)c1)[i];
+
+      write(1,c1_out, n1);
     }
-    if (i < count)
-       ((char *)c1_out)[i] = ((char *)c1)[i];
-
-    write(1,c1_out, count);
- 
-    free(c1);
-    free(c1_out);
     return 0;
 }
